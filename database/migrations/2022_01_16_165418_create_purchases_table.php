ce5711a7c399b9356ba81bfd840da7b08e0d7d5c
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2022-01-16 13:03:23 -0400

    Agregando compras

---
+++ database/migrations/2022_01_16_165418_create_purchases_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('client_id');
+
+            $table->decimal('amount', 13, 2)->nullable();
+
+            $table->boolean('is_paid')->default(false);
+
+            $table->timestamps();
+
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
