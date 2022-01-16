19f8c85faf54bc14393e574568cee01c8f18c947
Gustavo Escobar Cobos <gustavo.escobar@outlook.com>
2022-01-16 12:53:53 -0400

    Agregando productos

---
+++ database/migrations/2022_01_16_164354_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 64);
+            $table->string('description', 128)->nullable();
+            $table->decimal('price', 9, 2);
+
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
